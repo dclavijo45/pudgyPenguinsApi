05dede613173e0d3c1382e98b09b255632d2cd51
Daniel Clavijo Gonzalez <daniel.clavijo@itac.co>
2021-12-19 15:10:40 -0500

    Add: Config for heroku. Update: Add file setup for heroku

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ from os import getenv as env
 import random
 import string
 
-#App
-SECRET_KEY = ''.join(random.
-    choice(f"{string.ascii_uppercase}{string.punctuation}{string.ascii_letters}") 
-    for i in range(20))
-
+#Server
+SECRET_KEY = ''.join(random.choice(f"{string.ascii_uppercase}{string.punctuation}{string.ascii_letters}") for i in range(20))
+PORT = int(env('PORT', 33507))
+HOST = env('HOST', '0.0.0.0')
 DEBUG = int(env('DEBUG', '1'))
 
+#App
 URL_BASE_IMAGES = "https://api.pudgypenguins.io/penguin/image/"
 LIST_NAMES = [
     'Bell', 'Woods', 'Sapphire', 'Summers', 'Phoenix', 'Cookies and Cream', 'Cuddles', 'Cupcake',
@@ -17,10 +17,6 @@ LIST_NAMES = [
 STATUS = [
     'Eating', 'Working', 'Playing', 'Resting'
 ]
-
-#Server
-PORT = int(env('PORT', 8100))
-HOST = env('HOST', '0.0.0.0')
 
 #MySQL
 MYSQL_HOST = env('mysql_host', 'localhost')
